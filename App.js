720faa29531a8d0184bc7d524f8666af3c0d641f
Conor Doyle <doyle7@hotmail.com>
2017-09-29 21:57:20 +0100

    Add Facebook login functionality via Expo

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Facebook } from 'expo';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+  async function logIn() {
+    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('178192322746471', {
+        permissions: ['public_profile'],
+      });
+    if (type === 'success') {
+      // Get the user's name using Facebook's Graph API
+      const response = await fetch(
+        `https://graph.facebook.com/me?access_token=${token}`);
+      Alert.alert(
+        'Logged in!',
+        `Hi ${(await response.json()).name}!`,
+      );
+    }
+  }
+  logIn();
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
