7348f6bbec96ada1f44a2da621ec78cc2fedd1e4
Conor Doyle <doyle7@hotmail.com>
2017-10-02 18:51:26 +0100

    Add 'PreSplash' component

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { bool } from 'prop-types';
 import { ReactModoroNavigator } from '~/containers';
+import { PreSplash } from '~/components';
 
 class App extends Component {
+  static propTypes = {
+    isAuthenticating: bool.isRequired
+  }
+
+  static defaultProps = {
+    isAuthenticating: true
+  }
+
   render() {
     return (
       <View style={{flex: 1}}>
-        <ReactModoroNavigator />
+        {this.props.isAuthenticating === true
+          ? <PreSplash />
+          : <ReactModoroNavigator />}
       </View>
     );
   }
