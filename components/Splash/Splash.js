5679c5ee3d424f0a3e2798307af40a7dbc8d4364
Conor Doyle <doyle7@hotmail.com>
2017-10-04 14:55:09 +0100

    Add styling for Splash screen

--- components/Splash/Splash.js
+++ components/Splash/Splash.js
@@ -1,16 +1,79 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { func } from 'prop-types';
+import { StyleSheet, Text, Image, View, Dimensions } from 'react-native';
+import { SocialIcon } from 'react-native-elements';
+import { colors, fontSizes } from '~/styles';
 
-class Splash extends Component {
-  render() {
-    return (
+const { height } = Dimensions.get('window');
+
+Splash.propTypes = {
+  login: func.isRequired
+};
+
+function Splash({ login }) {
+  return (
+    <View style={styles.container}>
       <View>
-        <Text>
-          Splash
-        </Text>
+        <Image style={styles.image} source={require('../../images/logo.png')} />
+        <Text style={styles.appTitle}>ReactModoro</Text>
       </View>
-    );
-  }
+      <View style={styles.loginContainer}>
+        <SocialIcon
+          style={styles.facebookLogin}
+          button
+          raised
+          type='facebook'
+          onLongPress={login}
+          title='Login with Facebook'
+        />
+        <Text style={styles.assuranceText}>{'Don\'t worry. We don\'t post anything to Facebook.'}</Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.white,
+    flex: 1,
+    paddingBottom: 40,
+    paddingTop: 50
+  },
+
+  appTitle: {
+    color: colors.blue,
+    fontSize: 40,
+    margin: 10,
+    textAlign: 'center'
+  },
+
+  image: {
+    resizeMode: 'contain',
+    height: height * 0.4 > 300 ? 300 : height * 0.4
+  },
+
+  loginContainer: {
+    paddingLeft: 30,
+    paddingRight: 30,
+    alignItems: 'center'
+  },
+
+  facebookLogin: {
+    height: 33,
+    width: 180,
+    justifyContent: 'center',
+    borderRadius: 3,
+    marginBottom: 13,
+    alignItems: 'center'
+  },
+
+  assuranceText: {
+    textAlign: 'center',
+    color: colors.secondary,
+    fontSize: fontSizes.secondary
+  }
+});
 
 export default Splash;
--- containers/Splash/SplashContainer.js
+++ containers/Splash/SplashContainer.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Facebook } from 'expo';
+import { Alert } from 'react-native';
 import { Splash } from '~/components';
 
 class SplashContainer extends Component {
+  login = async () => {
+    const { type, token } = await Facebook.logInWithReadPermissionsAsync('178192322746471', {
+      permissions: ['public_profile']
+    });
+
+    if (type === 'success') {
+      // Get the user's name using Facebook's Graph API
+      const response = await fetch(
+        `https://graph.facebook.com/me?access_token=${token}`);
+      Alert.alert(
+        'Logged in!',
+        `Hi ${(await response.json()).name}!`,
+      );
+    }
+  }
+
   render() {
     return (
-      <Splash />
+      <Splash login={this.login} />
     );
   }
 }
