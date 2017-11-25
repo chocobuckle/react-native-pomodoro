2c1ecb9482f1b78524159e58561d9ed4488bf477
Conor Doyle <doyle7@hotmail.com>
2017-11-25 20:48:06 +0000

    Add/style UI for Settings view

--- components/Settings/Settings.js
+++ components/Settings/Settings.js
@@ -1,23 +1,61 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 import { ReactModoroNavbar, Close } from '~/components';
-import { colors } from '~/styles';
+import { colors, fontSizes } from '~/styles';
+import Slider from 'react-native-slider';
 
 Settings.propTypes = {
-  onBack: PropTypes.func.isRequired
+  onBack: PropTypes.func.isRequired,
+  timerDuration: PropTypes.number.isRequired,
+  restDuration: PropTypes.number.isRequired,
+  onTimerChange: PropTypes.func.isRequired,
+  onRestChange: PropTypes.func.isRequired,
+  onRestComplete: PropTypes.func.isRequired,
+  onTimerComplete: PropTypes.func.isRequired,
+  onLogout: PropTypes.func.isRequired
 };
 
 export default function Settings(props) {
   return (
-    <View>
+    <View style={styles.container} >
       <ReactModoroNavbar
         title='Settings'
         leftButton={<Close onPress={props.onBack} />}
       />
-      <Text>
-        Settings
-      </Text>
+      <View style={styles.sliderContainer}>
+        <Text style={styles.titleText}>Timer Duration</Text>
+        <Text style={styles.valueText}>{props.timerDuration}</Text>
+        <Text style={styles.minutes}>{props.timerDuration === 1 ? 'Minute' : 'Minutes'}</Text>
+        <Slider
+          minimumValue={1}
+          maximumValue={60}
+          onSlidingComplete={props.onTimerComplete}
+          thumbTintColor={colors.border}
+          step={1}
+          minimumTrackTintColor={colors.blue}
+          value={props.timerDuration}
+          onValueChange={props.onTimerChange}
+        />
+      </View>
+      <View style={styles.sliderContainer}>
+        <Text style={styles.titleText}>Rest Duration</Text>
+        <Text style={styles.valueText}>{props.restDuration}</Text>
+        <Text style={styles.minutes}>{props.restDuration === 1 ? 'Minute' : 'Minutes'}</Text>
+        <Slider
+          minimumValue={1}
+          maximumValue={60}
+          onSlidingComplete={props.onRestComplete}
+          thumbTintColor={colors.border}
+          step={1}
+          minimumTrackTintColor={colors.blue}
+          value={props.restDuration}
+          onValueChange={props.onRestChange}
+        />
+      </View>
+      <TouchableOpacity onPress={props.onLogout} style={styles.logout}>
+        <Text style={styles.logoutText}>Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -26,5 +64,39 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.white
+  },
+  sliderContainer: {
+    flex: 1,
+    marginLeft: 20,
+    marginRight: 20,
+    alignItems: 'stretch',
+    justifyContent: 'center'
+  },
+  titleText: {
+    fontSize: 20,
+    color: colors.primary,
+    textAlign: 'center'
+  },
+  valueText: {
+    fontSize: 50,
+    color: colors.blue,
+    textAlign: 'center',
+    padding: 15
+  },
+  minutes: {
+    color: colors.secondary,
+    textAlign: 'center'
+  },
+  logout: {
+    backgroundColor: colors.blue,
+    alignItems: 'stretch',
+    borderRadius: 25,
+    margin: 25,
+    padding: 10
+  },
+  logoutText: {
+    color: colors.white,
+    fontSize: fontSizes.secondary,
+    textAlign: 'center'
   }
 });
--- containers/Settings/SettingsContainer.js
+++ containers/Settings/SettingsContainer.js
@@ -8,9 +8,43 @@ export default class SettingsContainer extends Component {
     navigator: PropTypes.object.isRequired
   }
 
+  state={
+    timerDuration: 20,
+    restDuration: 5
+  }
+
+  handleTimerChange = timerDuration => {
+    this.setState({ timerDuration });
+  }
+
+  handleRestChange = restDuration => {
+    this.setState({ restDuration });
+  }
+
+  handleTimerComplete = () => {
+    console.log('Finished sliding timer');
+  }
+
+  handleRestComplete = () => {
+    console.log('Finished sliding rest');
+  }
+
+  handleLogout = () => {
+    console.log('Logging out!');
+  }
+
   render() {
     return (
-      <Settings onBack={this.props.navigator.pop} />
+      <Settings
+        onBack={this.props.navigator.pop}
+        onLogout={this.handleLogout}
+        onRestComplete={this.handleRestComplete}
+        onTimerComplete={this.handleTimerComplete}
+        onTimerChange={this.handleTimerChange}
+        onRestChange={this.handleRestChange}
+        timerDuration={this.state.timerDuration}
+        restDuration={this.state.restDuration}
+      />
     );
   }
 }
